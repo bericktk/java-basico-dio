d0ec95c8a518417c3fff1c47d9f3cfcb1ad7bea1
bericktk <br.ericktk@gmail.com>
2025-01-16 21:08:21 -0300

    Realizando uma condição com a idade.

---
+++ firts-project-java/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+    public static void main(String[] args){
+        
+        String nome = "Jamylle";
+        int idade = 22;
+
+        if (idade < 80) {
+            System.out.println("Você é jovem!");
+        } else {
+            System.out.println("Você é idoso!");
+            
+        }
+    }
+}
