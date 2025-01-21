bae2069db43668226a7dc0365de0106897572026
bericktk <br.ericktk@gmail.com>
2025-01-20 21:28:27 -0300

    Aprendendo sobre métodos das classs em JAVA.

--- firts-project-java/src/MinhaClasse.java
+++ firts-project-java/src/MinhaClasse.java
@@ -1,14 +1,14 @@
 public class MinhaClasse {
     public static void main(String[] args){
-        
-        String nome = "Jamylle";
-        int idade = 22;
+        String primeiroNome = "Jamylle";
+        String segundoNome = "Amaral";
 
-        if (idade < 80) {
-            System.out.println("Você é jovem!");
-        } else {
-            System.out.println("Você é idoso!");
-            
-        }
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método" + " " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
